24c78e48a00681a1867d2397c7161ba1eeb24d66
James Beilsten-Edmands <30625594+jbeilstenedmands@users.noreply.github.com>
2024-09-06 16:20:25 +0100

    Passing flood fill tests

--- src/dials/algorithms/indexing/fastindex.h
+++ src/dials/algorithms/indexing/fastindex.h
@@ -252,8 +252,9 @@ scitbx::af::shared<scitbx::vec3<double>> do_floodfill(scitbx::af::shared<double>
                                                       double d_min = 1.8) {
   auto start = std::chrono::system_clock::now();
   int n_points = 256;
-  // double fft_cell_length = n_points * d_min / 2;
-  // first calc rmsd and use this to create a binary grid
+  // int n_points = 256;
+  //  double fft_cell_length = n_points * d_min / 2;
+  //  first calc rmsd and use this to create a binary grid
   double sumg = 0.0;
   for (int i = 0; i < grid.size(); ++i) {
     sumg += grid[i];
@@ -264,7 +265,7 @@ scitbx::af::shared<scitbx::vec3<double>> do_floodfill(scitbx::af::shared<double>
     sum_delta_sq += std::pow(grid[i] - meang, 2);
   }
   double rmsd = std::pow(sum_delta_sq / grid.size(), 0.5);
-  scitbx::af::shared<int> grid_binary(256 * 256 * 256, 0);
+  scitbx::af::shared<int> grid_binary(n_points * n_points * n_points, 0);
   double cutoff = rmsd_cutoff * rmsd;
 
   for (int i = 0; i < grid.size(); i++) {
@@ -276,8 +277,8 @@ scitbx::af::shared<scitbx::vec3<double>> do_floodfill(scitbx::af::shared<double>
   // now do flood fill
   int n_voids = 0;
 
-  std::stack<int> stack;
-  std::vector<std::vector<int>> accumulators;
+  std::stack<scitbx::vec3<int>> stack;
+  std::vector<std::vector<scitbx::vec3<int>>> accumulators;
   int target = 1;
   int replacement = 2;
   std::vector<int> grid_points_per_void;
@@ -289,101 +290,110 @@ scitbx::af::shared<scitbx::vec3<double>> do_floodfill(scitbx::af::shared<double>
 
   for (int i = 0; i < grid_binary.size(); i++) {
     if (grid_binary[i] == target) {
-      stack.push(i);
+      // std::cout << i << std::endl;
+      int x = i % n_points;
+      int y = (i % n_sq) / n_points;
+      int z = i / n_sq;
+      scitbx::vec3<int> xyz = {x, y, z};
+      stack.push(xyz);
       grid_binary[i] = replacement;
-      std::vector<int> this_accumulator;
+      std::vector<scitbx::vec3<int>> this_accumulator;
       accumulators.push_back(this_accumulator);
       n_voids++;
       grid_points_per_void.push_back(0);
-      int x_plus = 0;
-      int x_minus = 0;
-      int y_plus = 0;
-      int y_minus = 0;
-      int z_plus = 0;
-      int z_minus = 0;
+
       while (!stack.empty()) {
-        int index = stack.top();
+        scitbx::vec3<int> this_xyz = stack.top();
         stack.pop();
-        accumulators[accumulator_index].push_back(index);
+        accumulators[accumulator_index].push_back(this_xyz);
         grid_points_per_void[accumulator_index]++;
         // when finding nearest neighbours, need to check we don't step over the edge in
         // each dimension likely not very efficient right now!
-        if ((index + 1) % n_points != 0) {
-          x_plus = index + 1;
-        } else {
-          x_plus = index + 1 - n_points;
+        // std::cout << "index " << index << std::endl;
+        /*while (index < 0){
+            index += total;
         }
-        if (grid_binary[x_plus] == target) {
-          grid_binary[x_plus] = replacement;
-          stack.push(x_plus);
+        while (index >= total) {
+            index -= total;
+        }*/
+
+        // std::cout << "xyz " << x << " " << y << " " << z << std::endl;
+        //  increment x and calculate the array index
+        // std::cout << "xyz " << this_xyz[0] << " " << this_xyz[1] << " " <<
+        // this_xyz[2] << std::endl;
+        int x_plus = this_xyz[0] + 1;
+        int modx = modulo(this_xyz[0], n_points);
+        int mody = modulo(this_xyz[1], n_points) * n_points;
+        int modz = modulo(this_xyz[2], n_points) * n_sq;
+        int array_index = modulo(x_plus, n_points) + mody + modz;
+        /// std::cout << "xplus idx " << array_index << std::endl;
+        if (grid_binary[array_index] == target) {
+          grid_binary[array_index] = replacement;
+          scitbx::vec3<int> new_xyz = {x_plus, this_xyz[1], this_xyz[2]};
+          stack.push(new_xyz);
         }
-        if (index % n_points != 0) {
-          x_minus = index - 1;
-        } else {
-          x_minus = index - 1 + n_points;
+        int x_minus = this_xyz[0] - 1;
+        array_index = modulo(x_minus, n_points) + mody + modz;
+        /// std::cout << "xminus idx " << marray_index << std::endl;
+        if (grid_binary[array_index] == target) {
+          grid_binary[array_index] = replacement;
+          scitbx::vec3<int> new_xyz = {x_minus, this_xyz[1], this_xyz[2]};
+          stack.push(new_xyz);
         }
-        if (grid_binary[x_minus] == target) {
-          grid_binary[x_minus] = replacement;
-          stack.push(x_minus);
+
+        int y_plus = this_xyz[1] + 1;
+        array_index = modx + (modulo(y_plus, n_points) * n_points) + modz;
+        /// std::cout << "xplus idx " << array_index << std::endl;
+        if (grid_binary[array_index] == target) {
+          grid_binary[array_index] = replacement;
+          scitbx::vec3<int> new_xyz = {this_xyz[0], y_plus, this_xyz[2]};
+          stack.push(new_xyz);
         }
-        if ((index % n_sq) < n_sq_minus_n) {
-          y_plus = index + n_points;
-        } else {
-          y_plus = index + n_points - n_sq;
+        int y_minus = this_xyz[1] - 1;
+        array_index = modx + (modulo(y_minus, n_points) * n_points) + modz;
+        /// std::cout << "xminus idx " << marray_index << std::endl;
+        if (grid_binary[array_index] == target) {
+          grid_binary[array_index] = replacement;
+          scitbx::vec3<int> new_xyz = {this_xyz[0], y_minus, this_xyz[2]};
+          stack.push(new_xyz);
         }
-        if (grid_binary[y_plus] == target) {
-          grid_binary[y_plus] = replacement;
-          stack.push(y_plus);
+
+        int z_plus = this_xyz[2] + 1;
+        array_index = modx + mody + (modulo(z_plus, n_points) * n_sq);
+        /// std::cout << "xplus idx " << array_index << std::endl;
+        if (grid_binary[array_index] == target) {
+          grid_binary[array_index] = replacement;
+          scitbx::vec3<int> new_xyz = {this_xyz[0], this_xyz[1], z_plus};
+          stack.push(new_xyz);
         }
-        if ((index % n_sq) >= n_points) {
-          y_minus = index - n_points;
-        } else {
-          y_minus = index - n_points + n_sq;
-        }
-        if (grid_binary[y_minus] == target) {
-          grid_binary[y_minus] = replacement;
-          stack.push(y_minus);
-        }
-        if ((index % total) < nn_sq_minus_n) {
-          z_plus = index + n_sq;
-        } else {
-          z_plus = index + n_sq - total;
-        }
-        if (grid_binary[z_plus] == target) {
-          grid_binary[z_plus] = replacement;
-          stack.push(z_plus);
-        }
-        if ((index % total) >= n_sq) {
-          z_minus = index - n_sq;
-        } else {
-          z_minus = index - n_sq + total;
-        }
-        if (grid_binary[z_minus] == target) {
-          grid_binary[z_minus] = replacement;
-          stack.push(z_minus);
+        int z_minus = this_xyz[2] - 1;
+        array_index = modx + mody + (modulo(z_minus, n_points) * n_sq);
+        /// std::cout << "xminus idx " << marray_index << std::endl;
+        if (grid_binary[array_index] == target) {
+          grid_binary[array_index] = replacement;
+          scitbx::vec3<int> new_xyz = {this_xyz[0], this_xyz[1], z_minus};
+          stack.push(new_xyz);
         }
       }
       replacement++;
       accumulator_index++;
     }
   }
-  std::cout << "n voids cpp " << n_voids << std::endl;
-  // DIALS_ASSERT(n_voids > 3)
-
-  // want centres_of_mass_frac and grid_points_per_void
-  // now calc centres of mass.
   scitbx::af::shared<scitbx::vec3<double>> centres_of_mass_frac(n_voids);
   for (int i = 0; i < accumulators.size(); i++) {
-    std::vector<int> values = accumulators[i];
+    std::vector<scitbx::vec3<int>> values = accumulators[i];
     int n = values.size();
     int divisor = n * n_points;
     double x = 0.0;
     double y = 0.0;
     double z = 0.0;
     for (int j = 0; j < n; j++) {
-      x += (values[j] % n_points);
-      y += ((values[j] % n_sq) / n_points);
-      z += (values[j] / n_sq);
+      /*std::cout << values[j] << " " << (values[j] % n_points) << " "
+                << ((values[j] % n_sq) / n_points) << " " << (values[j] / n_sq)
+                << std::endl;*/
+      x += values[j][0];  // % n_points);
+      y += values[j][1];  // % n_sq) / n_points);
+      z += values[j][2];  // / n_sq);
     }
     x /= divisor;
     y /= divisor;
--- src/dials/algorithms/indexing/floodfill.h
+++ src/dials/algorithms/indexing/floodfill.h
@@ -14,6 +14,10 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 
+int modulo(int i, int n) {
+  return (i % n + n) % n;
+}
+
 std::tuple<scitbx::af::shared<int>,
            std::vector<int>,
            scitbx::af::shared<scitbx::vec3<double>>>
@@ -46,8 +50,8 @@ flood_fill(scitbx::af::shared<double> grid,
   // now do flood fill
   int n_voids = 0;
 
-  std::stack<int> stack;
-  std::vector<std::vector<int>> accumulators;
+  std::stack<scitbx::vec3<int>> stack;
+  std::vector<std::vector<scitbx::vec3<int>>> accumulators;
   int target = 1;
   int replacement = 2;
   std::vector<int> grid_points_per_void;
@@ -60,22 +64,21 @@ flood_fill(scitbx::af::shared<double> grid,
   for (int i = 0; i < grid_binary.size(); i++) {
     if (grid_binary[i] == target) {
       // std::cout << i << std::endl;
-      stack.push(i);
+      int x = i % n_points;
+      int y = (i % n_sq) / n_points;
+      int z = i / n_sq;
+      scitbx::vec3<int> xyz = {x, y, z};
+      stack.push(xyz);
       grid_binary[i] = replacement;
-      std::vector<int> this_accumulator;
+      std::vector<scitbx::vec3<int>> this_accumulator;
       accumulators.push_back(this_accumulator);
       n_voids++;
       grid_points_per_void.push_back(0);
-      int x_plus = 0;
-      int x_minus = 0;
-      int y_plus = 0;
-      int y_minus = 0;
-      int z_plus = 0;
-      int z_minus = 0;
+
       while (!stack.empty()) {
-        int index = stack.top();
+        scitbx::vec3<int> this_xyz = stack.top();
         stack.pop();
-        accumulators[accumulator_index].push_back(index);
+        accumulators[accumulator_index].push_back(this_xyz);
         grid_points_per_void[accumulator_index]++;
         // when finding nearest neighbours, need to check we don't step over the edge in
         // each dimension likely not very efficient right now!
@@ -86,63 +89,63 @@ flood_fill(scitbx::af::shared<double> grid,
         while (index >= total) {
             index -= total;
         }*/
-        if ((index + 1) % n_points != 0) {  // if not on top boundary
-          x_plus = index + 1;
-        } else {
-          x_plus = index + 1 - n_points;
+
+        // std::cout << "xyz " << x << " " << y << " " << z << std::endl;
+        //  increment x and calculate the array index
+        // std::cout << "xyz " << this_xyz[0] << " " << this_xyz[1] << " " <<
+        // this_xyz[2] << std::endl;
+        int x_plus = this_xyz[0] + 1;
+        int modx = modulo(this_xyz[0], n_points);
+        int mody = modulo(this_xyz[1], n_points) * n_points;
+        int modz = modulo(this_xyz[2], n_points) * n_sq;
+        int array_index = modulo(x_plus, n_points) + mody + modz;
+        /// std::cout << "xplus idx " << array_index << std::endl;
+        if (grid_binary[array_index] == target) {
+          grid_binary[array_index] = replacement;
+          scitbx::vec3<int> new_xyz = {x_plus, this_xyz[1], this_xyz[2]};
+          stack.push(new_xyz);
         }
-        std::cout << x_plus << std::endl;
-        if (grid_binary[x_plus] == target) {
-          grid_binary[x_plus] = replacement;
-          stack.push(index + 1);
+        int x_minus = this_xyz[0] - 1;
+        array_index = modulo(x_minus, n_points) + mody + modz;
+        /// std::cout << "xminus idx " << marray_index << std::endl;
+        if (grid_binary[array_index] == target) {
+          grid_binary[array_index] = replacement;
+          scitbx::vec3<int> new_xyz = {x_minus, this_xyz[1], this_xyz[2]};
+          stack.push(new_xyz);
         }
-        //}
-        if (index % n_points != 0) {
-          x_minus = index - 1;
-        } else {
-          x_minus = index - 1 + n_points;
+
+        int y_plus = this_xyz[1] + 1;
+        array_index = modx + (modulo(y_plus, n_points) * n_points) + modz;
+        /// std::cout << "xplus idx " << array_index << std::endl;
+        if (grid_binary[array_index] == target) {
+          grid_binary[array_index] = replacement;
+          scitbx::vec3<int> new_xyz = {this_xyz[0], y_plus, this_xyz[2]};
+          stack.push(new_xyz);
         }
-        std::cout << x_minus << std::endl;
-        if (grid_binary[x_minus] == target) {
-          grid_binary[x_minus] = replacement;
-          stack.push(index - 1);
+        int y_minus = this_xyz[1] - 1;
+        array_index = modx + (modulo(y_minus, n_points) * n_points) + modz;
+        /// std::cout << "xminus idx " << marray_index << std::endl;
+        if (grid_binary[array_index] == target) {
+          grid_binary[array_index] = replacement;
+          scitbx::vec3<int> new_xyz = {this_xyz[0], y_minus, this_xyz[2]};
+          stack.push(new_xyz);
         }
-        //}
-        if ((index % n_sq) < n_sq_minus_n) {
-          y_plus = index + n_points;
-        } else {
-          y_plus = index + n_points - n_sq;
+
+        int z_plus = this_xyz[2] + 1;
+        array_index = modx + mody + (modulo(z_plus, n_points) * n_sq);
+        /// std::cout << "xplus idx " << array_index << std::endl;
+        if (grid_binary[array_index] == target) {
+          grid_binary[array_index] = replacement;
+          scitbx::vec3<int> new_xyz = {this_xyz[0], this_xyz[1], z_plus};
+          stack.push(new_xyz);
         }
-        if (grid_binary[y_plus] == target) {
-          grid_binary[y_plus] = replacement;
-          stack.push(index + n_points);
-        }
-        if ((index % n_sq) >= n_points) {
-          y_minus = index - n_points;
-        } else {
-          y_minus = index - n_points + n_sq;
-        }
-        if (grid_binary[y_minus] == target) {
-          grid_binary[y_minus] = replacement;
-          stack.push(index - n_points);
-        }
-        if ((index % total) < nn_sq_minus_n) {
-          z_plus = index + n_sq;
-        } else {
-          z_plus = index + n_sq - total;
-        }
-        if (grid_binary[z_plus] == target) {
-          grid_binary[z_plus] = replacement;
-          stack.push(index + n_sq);
-        }
-        if ((index % total) >= n_sq) {
-          z_minus = index - n_sq;
-        } else {
-          z_minus = index - n_sq + total;
-        }
-        if (grid_binary[z_minus] == target) {
-          grid_binary[z_minus] = replacement;
-          stack.push(index - n_sq);
+        int z_minus = this_xyz[2] - 1;
+        array_index = modx + mody + (modulo(z_minus, n_points) * n_sq);
+        /// std::cout << "xminus idx " << marray_index << std::endl;
+        if (grid_binary[array_index] == target) {
+          grid_binary[array_index] = replacement;
+          scitbx::vec3<int> new_xyz = {this_xyz[0], this_xyz[1], z_minus};
+          stack.push(new_xyz);
         }
       }
       replacement++;
@@ -151,19 +154,19 @@ flood_fill(scitbx::af::shared<double> grid,
   }
   scitbx::af::shared<scitbx::vec3<double>> centres_of_mass_frac(n_voids);
   for (int i = 0; i < accumulators.size(); i++) {
-    std::vector<int> values = accumulators[i];
+    std::vector<scitbx::vec3<int>> values = accumulators[i];
     int n = values.size();
-    int divisor = n;  // * n_points;
+    int divisor = n * n_points;
     double x = 0.0;
     double y = 0.0;
     double z = 0.0;
     for (int j = 0; j < n; j++) {
-      std::cout << values[j] << " " << (values[j] % n_points) << " "
+      /*std::cout << values[j] << " " << (values[j] % n_points) << " "
                 << ((values[j] % n_sq) / n_points) << " " << (values[j] / n_sq)
-                << std::endl;
-      x += (values[j] % n_points);
-      y += ((values[j] % n_sq) / n_points);
-      z += (values[j] / n_sq);
+                << std::endl;*/
+      x += values[j][0];  // % n_points);
+      y += values[j][1];  // % n_sq) / n_points);
+      z += values[j][2];  // / n_sq);
     }
     x /= divisor;
     y /= divisor;
